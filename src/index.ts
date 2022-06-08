a5b78bf965ec6f30e4fdf0c5845ca257146795f5
Kamyx001 <Kamyx001@gmail.com>
2022-06-08 15:00:10 +0200

    Tracker

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import Tracker from "./Tracker";
 import apiKeysJSON from './apiKeys.json';
+import PlayerList from "./PlayerList";
 
 
-const tracker = new Tracker(apiKeysJSON);
+const players = new PlayerList();
+
+const tracker = new Tracker(apiKeysJSON, players);
 
 tracker.initialize();
